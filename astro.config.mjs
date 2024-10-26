e53c301fe50888390b8a3eea0bad946849542ea4
KP <prasanth.25k@gmail.com>
2024-10-26 12:12:25 +0530

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://prasanthkarri.dev",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
